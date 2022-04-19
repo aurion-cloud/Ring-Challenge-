ad1f62f1185d5b03d75e88e2fc792dc6ca8453ef
aurion-cloud <aurion.howard@cloudreach.com>
2022-04-19 10:10:07 -0400

    cronjob updates

--- cronjob.sh
+++ cronjob.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
-
-python3 /home/ec2-user/number_increment.py
-cp /home/ec2-user/index.html /usr/share/nginx/html/index.html
+#!/bin/sh 
+while true
+do
+  sudo python3 /home/ec2-user/number_increment.py
+  sudo cp /home/ec2-user/index.html /usr/share/nginx/html/index.html
+  sleep 10 
+done
