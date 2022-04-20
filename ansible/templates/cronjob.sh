4c5dde04b78556777bde34643ca31cd301c831e6
yacinebeye <yacine.beye@cloudreach.com>
2022-04-20 00:50:07 -0400

    lates updates to the cronjob.sh file to automate the IP ADDRESS look up

--- ansible/templates/cronjob.sh
+++ ansible/templates/cronjob.sh
@@ -19,4 +19,17 @@ do
   sudo cp /home/ec2-user/index.html /usr/share/nginx/html/index.html
   #scp -i ~/.ssh/aws_keypair.pem /home/ec2-user/index.html ec2-user@:54.167.48.13 ~/
   sleep 10
-done 
+
+
+#!/bin/sh
+IPS='54.146.163.250 3.22.186.56 54.242.145.132 3.92.25.77'
+if [[ ! -f ${IP_FILE} ]]; then
+  echo "No File!"
+  exit 1
+fi
+for IP_ADDRESS in ${IPS}; do
+  echo "TEST FOR ${IP_ADDRESS}"
+  # The -c 1 means send one packet, and the -t 1 means a 1 second timeout    
+  ping -c 1 -t 10 ${IP_ADDRESS} >> ${IP_ADDRESS}.log 2>&1 
+  sleep 10
+done
